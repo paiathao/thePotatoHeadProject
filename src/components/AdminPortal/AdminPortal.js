22ce5ae8376a6b8d62c6c6ec8f4f6487cb098d0b
Jimmy Brannon <user.email>
2018-09-07 16:27:54 -0500

    update key names in defaultprops

--- src/components/AdminPortal/AdminPortal.js
+++ src/components/AdminPortal/AdminPortal.js
@@ -80,30 +80,22 @@ class AdminPortal extends Component {
   }
 }
 
+
 AdminPortal.defaultProps = {
   data: [
     {
       id: 1,
-      baby: [{ first: 'Jerry', last: 'Smite', dob: '11/22/17', gender: 'boy', weeks: 22, days: 0, pounds: 4, ounces: 8 }],
-      nominatorName: 'Jimmy',
-      nominatorEmail: 'jimmy@jimmy.com',
-      parentName: 'Jean And Gary',
-      parentEmail: 'jean@jean.com',
-      hospitalName: 'MSP NCIU',
-      address: '123 Fake Hosptial St.',
-      address2: null,
-      city: 'Minneapolis',
-      state: 'MN',
-      zip: '55405',
-      country: 'USA',  
-      personalNote: 'I hope everything goes well for everyone!!!!',
-      subscription: true,
-    },
-    {
-      id: 2,
       baby: [
-        { first: 'Jerry', last: 'Smite', dob: '11/22/17', gender: 'boy', weeks: 22, days: 0, pounds: 4, ounces: 8 },
-        { first: 'Jerry', last: 'Smite', dob: '11/22/17', gender: 'boy', weeks: 22, days: 0, pounds: 4, ounces: 8 }
+        { 
+          firstName: 'Jerry', 
+          lastName: 'Smite', 
+          birthDate: '11/22/17', 
+          gender: 'boy', 
+          gestationWeeks: 22, 
+          gestationDays: 0, 
+          weightPounds: 4, 
+          weightOunces: 8 
+        }
       ],
       nominatorName: 'Jimmy',
       nominatorEmail: 'jimmy@jimmy.com',
