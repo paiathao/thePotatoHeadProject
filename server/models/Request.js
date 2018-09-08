6a235dd24e67bee0ef067b6b4ac6f009aac703f0
Jimmy Brannon <user.email>
2018-09-08 16:50:51 -0500

    add city key to schema

--- server/models/Request.js
+++ server/models/Request.js
@@ -12,6 +12,7 @@ const RequestSchema = new Schema({
     hospitalName: { type: String, required: true },
     address: { type: String, required: true },
     address2: { type: String},
+    city: { type: String, required: true },
     state: { type: String, required: true },
     zip: { type: String, required: true },
     country: { type: String, required: true },  
