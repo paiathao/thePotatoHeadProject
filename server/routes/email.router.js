8eb1699cc0778dc08ddface5b051c2de7ec4427c
Paia Thao <paiathao09@gmail.com>
2018-09-11 14:15:19 -0500

    update resetLink in email.router to make it easier to read

--- server/routes/email.router.js
+++ server/routes/email.router.js
@@ -58,10 +58,8 @@ router.get('/reset', (req, res) => {
                     to: process.env.CLIENT_USER
                 },
                 locals: {
-                    note: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-                        'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                        'http://' + 'localhost:3000/#' + '/reset-password/' + token + '\n\n' +
-                        'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+                    resetLink: 
+                        `http://localhost:3000/#/reset-password/${token}` 
                 },
             })
                 .then(console.log)
